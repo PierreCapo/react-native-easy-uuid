1682c0ab7d4c589aa2d73104963c87b28f4fd297
PierreCapo <pierre.caporossi@gadz.org>
2024-08-06 10:05:34 +0200

    prune: remove old arch

--- android/src/newarch/EasyUuidSpec.kt
+++
@@ -1,7 +0,0 @@
-package com.easyuuid
-
-import com.facebook.react.bridge.ReactApplicationContext
-
-abstract class EasyUuidSpec internal constructor(context: ReactApplicationContext) :
-  NativeEasyUuidSpec(context) {
-}
--- example/ios/File.swift
+++
@@ -1,6 +0,0 @@
-//
-//  File.swift
-//  EasyUuidExample
-//
-
-import Foundation
